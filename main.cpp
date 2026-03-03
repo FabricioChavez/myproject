54764b76ceb26d08474c6140c3c8830eda2cf6ee
Fabricio <fabriciochba@gmail.com>
2026-03-03 01:22:13 -0500

    Test Branch protection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
+int suma(int a, int b) {
+    return a + b;
+}
 
-int main(){
-    cout<<"FIRST TRIAL FOR GIT ACTIONS ..."<<endl;
-    return 0;
+int raiz_entera(int x) {
+    if (x < 0)
+        return -1;  // error simple
+
+    int r = 0;
+    while (r * r <= x) {
+        r++;
+    }
+    return r - 1;
 }
--- test.cpp
+++ test.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
 #include <cassert>
 
-int sumar(int a, int b) {
-    return a + b;
-}
+// Declaraciones
+int suma(int a, int b);
+int raiz_entera(int x);
 
 int main() {
-    assert(sumar(2, 3) == 5);
-    assert(sumar(0, 0) == 0);
-    assert(sumar(-1, 1) == 0);
-    std::cout << "Todos los tests pasaron!" << std::endl;
+
+    // Tests suma
+    assert(suma(2,3) == 5);
+    assert(suma(-1,1) == 0);
+
+    // Tests raiz
+    assert(raiz_entera(0) == 0);
+    assert(raiz_entera(4) == 2);
+    assert(raiz_entera(10) == 3);
+    assert(raiz_entera(-5) == -1);
+
     return 0;
 }
